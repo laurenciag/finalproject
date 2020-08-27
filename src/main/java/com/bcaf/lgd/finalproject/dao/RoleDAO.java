6f033d504795a01f4992434248f08e11150b367c
laurenciag <laurenciagdp@gmail.com>
2020-08-27 13:26:42 +0700

    update

--- src/main/java/com/bcaf/lgd/finalproject/dao/RoleDAO.java
+++ src/main/java/com/bcaf/lgd/finalproject/dao/RoleDAO.java
@@ -1,4 +1,7 @@
 package com.bcaf.lgd.finalproject.dao;
 
-public interface RoleDAO {
+import com.bcaf.lgd.finalproject.Entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleDAO extends JpaRepository<Role, String> {
 }
--- src/main/java/com/bcaf/lgd/finalproject/dao/UserDAO.java
+++ src/main/java/com/bcaf/lgd/finalproject/dao/UserDAO.java
@@ -2,6 +2,8 @@ package com.bcaf.lgd.finalproject.dao;
 
 import com.bcaf.lgd.finalproject.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +11,7 @@ import java.util.Optional;
 @Repository
 public interface UserDAO extends JpaRepository<User, String> {
 //    Optional<com.bcaf.lgd.finalproject.Entity.User> findByEmail(String, email);
+
+    @Query ("SELECT email FROM User WHERE email = :email")
+    public User getEmail(@Param("email") String email);
 }
